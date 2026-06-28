aef3f6a2d03630945be0c76b6abcb18d56492f74
kingjamesncalimag-png <kingjames.n.calimag@isu.edu.ph>
2026-06-29 00:46:51 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,13 +1,27 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "smart_home",
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
+
+(async () => {
+    try {
+        const conn = await db.getConnection();
+        console.log("✅ Connected to Railway MySQL");
+        conn.release();
+    } catch (err) {
+        console.error("❌ Database connection failed:");
+        console.error(err.message);
+    }
+})();
 
 module.exports = db;
